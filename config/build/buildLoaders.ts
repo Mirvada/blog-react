b7bf4e0bb68ae097c1cce9dd8435f2caf811a4c4
Aleksandra <mirvada1@yandex.ru>
2026-03-25 16:15:21 +0300

    добавляет конфигурацию webpack

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,24 @@
+import { RuleSetRule } from 'webpack';
+
+export function buildLoaders(): RuleSetRule[] {
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      "style-loader",
+      "css-loader",
+      "sass-loader",
+    ],
+  };
+
+  
+  return [
+    typescriptLoader,
+    cssLoader,
+  ]
+}
