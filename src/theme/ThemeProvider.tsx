0231d24616a0053c37015301b5d981be954cc8df
Aleksandra <mirvada1@yandex.ru>
2026-03-27 15:30:53 +0300

    удаляет неиспользуемую функцию

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -10,10 +10,6 @@ const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || T
 const ThemeProvider = ({ children }: ThemeProviderProps) => {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
 
-  const toggleTheme = () => {
-    setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
-  }
-
   const defaultProps = useMemo(() => ({
     theme: theme,
     setTheme: setTheme,
