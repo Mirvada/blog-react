29c0cc181da8698ad3ce7608cd09ef00d988aab5
Aleksandra <mirvada1@yandex.ru>
2026-03-27 15:31:36 +0300

    добавляет clsx и helper

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Link, Route, Routes } from "react-router";
 import { MainPageLazy } from "./pages/MainPage/MainPageLazy";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPageLazy";
 import { useTheme } from './theme/useTheme';
+import { cn } from './shared/lib/cn';
 import './styles/index.scss'
 
 export function App() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={cn('app', theme)}>
       <button onClick={toggleTheme}>Theme</button>
       <Link to='/'>Главная</Link>
       <Link to='/about'>О сайте</Link>
