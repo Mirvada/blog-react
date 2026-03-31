422947a87b6017da4b8f47e4df7463324bde0d6b
Aleksandra <mirvada1@yandex.ru>
2026-03-31 15:30:00 +0300

    добавляет типы для изображений

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -9,3 +9,13 @@ declare module '*.module.scss'  {
   const  classes : {  [ key : string ] : string  } ; 
   export  default  classes ; 
 }
+
+declare module '*.svg' {
+  import React = require('react');
+  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.png'
