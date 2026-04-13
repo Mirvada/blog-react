9810085821fa85553ffe35dd3dbf257803e0cf61
Aleksandra <mirvada1@yandex.ru>
2026-04-13 20:42:14 +0300

    добавляет возможность использовать кнопку без выбора темы

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -6,11 +6,11 @@ import s from './Button.module.scss';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   children: ReactNode;
-  theme: ThemeButton;
+  theme?: ThemeButton;
 }
 
 export const Button = (props: ButtonProps) => {
-  const { className, children, theme, ...otherProps } = props;
+  const { className, children, theme = ThemeButton.CLEAR, ...otherProps } = props;
   return (
     <button
       type="button"
